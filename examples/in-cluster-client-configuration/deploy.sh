db3254b3705279feb06479981a05d3456784808d
Girish Kalele <gkalele@tetrationanalytics.com>
2024-07-15 11:19:22 -0700

    Turn off debugging

--- examples/in-cluster-client-configuration/deploy.sh
+++ examples/in-cluster-client-configuration/deploy.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash
 
 for i in `sudo /local/binaries/kubernetes/kubectl get node | awk '{ print $1 }'`;  do
-    rsync -av /tmp/probe.tar $i:/tmp/
+    rsync -a /tmp/probe.tar $i:/tmp/
 done
 for i in `sudo /local/binaries/kubernetes/kubectl get node | awk '{ print $1 }'`;  do
     ssh $i "sudo ctr -n k8s.io image import /tmp/probe.tar" & done
